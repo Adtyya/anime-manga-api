13c053fb8b8762a171a816015238002d3d25cac1
Aditya <aditdualapan@gmail.com>
2022-01-26 17:14:04 +0700

    up

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.get('/', (req, res)=> {
-  res.send('<h1>API Manga Reader By Aditya Fitriansyah</h1>');
+  res.send('<h1>Halo</h1>');
 })
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
